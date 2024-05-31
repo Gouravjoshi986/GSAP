c9708f959040de7767f5075e07149dd2f8fda7d4
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-05-31 10:04:43 +0530

    gsap basic timeline object

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
-gsap.to("#box1",{
-    x:800,
-    duration:2,
-    delay:0.5,
-    rotate:360,
-    backgroundColor:"lightseagreen",
-    borderRadius:"50%",
-    scale:0.5
-})
-gsap.from("#box2",{
-    x:800,
-    duration:2,
-    delay:0.5,
-})
-gsap.to("#box3",{
-    x:800,
-    duration:2,
-    delay:0.5,
-    rotate:360,
-    repeat:-1,
-    yoyo:true,
-})
+// gsap.to("#box1",{
+//     x:800,
+//     duration:2,
+//     delay:0.5,
+//     rotate:360,
+//     backgroundColor:"lightseagreen",
+//     borderRadius:"50%",
+//     scale:0.5
+// })
+// gsap.from("#box2",{
+//     x:800,
+//     duration:2,
+//     delay:0.5,
+// })
+// gsap.to("#box3",{
+//     x:800,
+//     duration:2,
+//     delay:0.5,
+//     rotate:360,
+//     repeat:-1,
+//     yoyo:true,
+// })
 
-gsap.from("h1",{
-    opacity:0,
-    duration:1,
-    y:30,
-    delay:1,
-    stagger:0.3
+// gsap.from("h1",{
+//     opacity:0,
+//     duration:1,
+//     y:30,
+//     delay:1,
+//     stagger:0.3
+// })
+
+var tl = gsap.timeline();
+
+tl.to("#box1",{
+    x:900,
+    rotate:360,
+    duration:1.5,
+    delay:1
+})
+tl.to("#box2",{
+    x:900,
+    backgroundColor:"yellow",
+    duration:1.5,
+})
+tl.to("#box3",{
+    x:900,
+    scale:0.5,
+    borderRadius:"50%",
+    duration:1.5,
 })
